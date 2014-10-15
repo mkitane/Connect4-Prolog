f86797e02ff60269daa7e784154824e827b9c953
mkitane <mehdikitane@gmail.com>
2014-10-15 16:51:06 +0200

    gestion du plateau en mode terminal

--- puissance.pl
+++ puissance.pl
@@ -7,14 +7,33 @@ bonjour(karim).
 bonjour(mimiettrititi).
 
 
-%Rule to Print the Matrix
-print_column([]).
-print_column([H|T]) :- H = a, ansi_format([bold, fg(red)], 'o', []),  ansi_format([bold, fg(blue)], ' | ', []), print_column(T), !.
-print_column([H|T]) :- H = b, ansi_format([bold, fg(green)], 'o', []),  ansi_format([bold, fg(blue)], ' | ', []), print_column(T), !.
-print_column([H|T]) :- H \= a, H \= b, ansi_format([bold, fg(green)], ' ', []),  ansi_format([bold, fg(blue)], ' | ', []), print_column(T), !.
 
-print_matrix([]).
-print_matrix([H|T]) :- print_column(H), nl, print_matrix(T).
+%Predicat pour dessiner soit le pion du joueur a en rouge, soit celui du joueur b en vert ou un espace si la case est vide
+print_element(H) :- H = a, ansi_format([bold, fg(red)], 'o', []),  ansi_format([bold, fg(blue)], ' | ', []).
+print_element(H) :- H = b, ansi_format([bold, fg(green)], 'o', []),  ansi_format([bold, fg(blue)], ' | ', []).
+print_element(H) :- H \= a, H \= b, ansi_format([bold, fg(green)], ' ', []),  ansi_format([bold, fg(blue)], ' | ', []).
+
+%Predicat qui permet de dessiner une ligne
+print_line(M,8,Y).
+print_line(M,X,Y) :-  nth1(X,M,Column), nth1(Y,Column,Elem), print_element(Elem), Xa is X+1, print_line(M,Xa,Y), !. 
+print_line(M,X,Y) :-  print_element(c) , Xa is X+1, print_line(M,Xa,Y), !. 
+
+%Predicat qui permet de dessiner toutes les colonnes. Il dessine les lignes une par une et redessine par recursion
+print_column(M,X,0).
+print_column(M,X,Y) :- print_line(M,X,Y), nl, Ya is Y - 1 , print_column(M,X,Ya), !.
+
+%Predicat qui gere le dessin du tableau
+print_matrix(M) :- X is 1, Y is 6, print_column(M,X,Y). 
+
+
+
+% print_column([]).
+% print_column([H|T]) :- H = a, ansi_format([bold, fg(red)], 'o', []),  ansi_format([bold, fg(blue)], ' | ', []), print_column(T), !.
+% print_column([H|T]) :- H = b, ansi_format([bold, fg(green)], 'o', []),  ansi_format([bold, fg(blue)], ' | ', []), print_column(T), !.
+% print_column([H|T]) :- H \= a, H \= b, ansi_format([bold, fg(green)], ' ', []),  ansi_format([bold, fg(blue)], ' | ', []), print_column(T), !.
+
+% print_matrix([]).
+% print_matrix([H|T]) :- print_column(H), nl, print_matrix(T).
 
 							
 
@@ -25,12 +44,12 @@ p([[],[],[],[],[],[],[]]).
 puissance :- bonjour(brahim).
 puissance :-
 	p(X),
-	write(X), nl,
 	write('>>'),
 	read(I),
 	addElement(a,I),
 	iarandom(X),
-	write(X),nl,
+	p(R),
+	print_matrix(R),
 	puissance.
 
 
