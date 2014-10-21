046954408576c624284e600d43a3260c62404e36
Karim <karim2904@gmail.com>
2014-10-21 20:35:15 +0200

    Fin jeu

--- finJeu.pl
+++ finJeu.pl
@@ -15,8 +15,6 @@ checkFinColonne(X,Y,L) :- S is 1, nth1(X,L,C), checkColonne(C,Y,S).
 
 
 % Fonction pour savoir si les 4 sur la meme ligne ont la meme couleur
-
-
 checkFinLigneBefore(X,Y,M,S, Compteur) :- 
 					Xb is X-1, 
 					nth1(X,M,La), nth1(Y,La,Couleur),
@@ -24,8 +22,6 @@ checkFinLigneBefore(X,Y,M,S, Compteur) :-
 					Sa is S + 1, 
 					checkFinLigneBefore(Xb,Y,M,Sa, Compteur), !.
 checkFinLigneBefore(_,_,_,S, Compteur) :- Compteur is S, !.
-		
-
 
 checkFinLigneAfter(X,Y,M,S, Compteur) :- 
 					Xb is X+1, 
@@ -40,10 +36,58 @@ checkFinLigne(X,Y,L) :-
 					Total is Sa+Sb+1,
 					write('Total Ligne:'), write(Total), nl,
 					Total >=4, !.
+					
+				
+				
+					
+% Fonction pour savoir si les 4 sur la meme diagonale ont la meme couleur
+checkFinDiagonaleBefore1(X,Y,M,S, Compteur) :- 
+					Xb is X-1, Yb is Y+1,
+					nth1(X,M,La), nth1(Y,La,Couleur),
+					nth1(Xb,M,Lb),nth1(Yb,Lb,Couleur),
+					Sa is S + 1, 
+					checkFinDiagonaleBefore1(Xb,Y,M,Sa, Compteur), !.
+checkFinDiagonaleBefore1(_,_,_,S, Compteur) :- Compteur is S, !.
+
+checkFinDiagonaleAfter1(X,Y,M,S, Compteur) :- 
+					Xb is X+1, Yb is Y-1
+					nth1(X,M,La), nth1(Y,La,Couleur),
+					nth1(Xb,M,Lb),nth1(Yb,Lb,Couleur),
+					Sa is S + 1, 
+					checkFinDiagonaleAfter1(Xb,Y,M,Sa, Compteur), !.
+checkFinDiagonaleAfter1(_,_,_,S, Compteur) :- Compteur is S, !.
+
+checkFinDiagonale1(X,Y,L) :- 
+					checkFinDiagonaleBefore1(X,Y,L,0,Sb), checkFinDiagonaleAfter1(X,Y,L,0,Sa),	
+					Total is Sa+Sb+1,
+					write('Total Diagonale:'), write(Total), nl,
+					Total >=4, !.
+					
+checkFinDiagonaleBefore2(X,Y,M,S, Compteur) :- 
+					Xb is X-1, Yb is Y-1,
+					nth1(X,M,La), nth1(Y,La,Couleur),
+					nth1(Xb,M,Lb),nth1(Yb,Lb,Couleur),
+					Sa is S + 1, 
+					checkFinDiagonaleBefore2(Xb,Yb,M,Sa, Compteur), !.
+checkFinDiagonaleBefore2(_,_,_,S, Compteur) :- Compteur is S, !.
+
+checkFinDiagonaleAfter2(X,Y,M,S, Compteur) :- 
+					Xb is X+1, Yb is Y+1
+					nth1(X,M,La), nth1(Y,La,Couleur),
+					nth1(Xb,M,Lb),nth1(Yb,Lb,Couleur),
+					Sa is S + 1, 
+					checkFinDiagonaleAfter2(Xb,Yb,M,Sa, Compteur), !.
+checkFinDiagonaleAfter2(_,_,_,S, Compteur) :- Compteur is S, !.
+
+checkFinDiagonale2(X,Y,L) :- 
+					checkFinDiagonaleBefore2(X,Y,L,0,Sb), checkFinDiagonaleAfter2(X,Y,L,0,Sa),	
+					Total is Sa+Sb+1,
+					write('Total Diagonale:'), write(Total), nl,
+					Total >=4, !.
 
 
 
 checkFinJeu(X,L) :-
 					nth1(X,L,Column),
 					matrice_utils:longueur(Column,Y),
-					(checkFinColonne(X,Y,L);checkFinLigne(X,Y,L)).
+					(checkFinColonne(X,Y,L);checkFinLigne(X,Y,L);checkFinDiagonale1(X,Y,L);checkFinDiagonale2(X,Y,L)).
