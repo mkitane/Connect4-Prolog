159eb39a72c78aea25be379eb7e4a1f2841c82ca
Karim <karim2904@gmail.com>
2014-10-23 16:46:58 +0200

    Unit tests

--- testFinJeu.pl
+++ testFinJeu.pl
@@ -1,35 +1,31 @@
 :-use_module(finJeu).
 
+:-begin_tests(finJeu).
+
 % ---------------- Test finColonne ----------------
 
-%true 
-%test1 :- checkFinColonne(1,5,[[a,a,a,a] [] [] [] [] []]).
-%false
-%test2 :- checkFinColonne(1,5,[[a,b,a,a] [] [] [] [] []]).
+test(t1) :- checkFinColonne(1,4,[[a,a,a,a],[],[],[],[],[]]).
+test(t2, [fail]) :- checkFinColonne(1,5,[[a,b,a,a],[],[],[],[],[]]).
 
 % ---------------- Test finLigne ----------------
 
-%true
-%test3 :- checkFinLigne(4,1,[[a,a,a,a] [a] [a] [a] [] []]).
-%false
-%test4 :- checkFinLigne(4,1,[[a,a,a,a] [a] [b] [a] [] []]).
+test(t3) :- checkFinLigne(4,1,[[a,a,a,a],[a],[a],[a],[],[]]).
+test(t4, [fail]) :- checkFinLigne(4,1,[[a,a,a,a],[a],[b],[a],[],[]]).
 
 % ---------------- Test finDiagonale1 ----------------
 
-%true
-test5 :- checkFinDiagonale1(1,4,[[a,a,a,a],[a,a,a],[a,a],[a],[],[]]).
-%false
-%test6 :- checkFinDiagonale1(1,4,[[a,a,b,a] [a,a,a] [a,a] [a] [] []]).
+test(t5) :- checkFinDiagonale1(1,4,[[a,a,a,a],[a,a,a],[a,a],[a],[],[]]).
+test(t6, [fail]) :- checkFinDiagonale1(1,4,[[a,a,a,a],[a,a,b,a],[a,a],[a],[],[]]).
 
 % ---------------- Test finDiagonale2 ----------------
 
-%true
-%test5 :- checkFinDiagonale2(4,4,[[a] [a,a] [a,a,a] [a,a,a,a] [] []]).
-%false
-%test6 :- checkFinDiagonale2(1,4,[[a,a,b,a] [a,a,a] [a,a] [a] [] []]).
+
+test(t7) :- checkFinDiagonale2(4,4,[[a],[a,a],[a,a,a],[a,a,a,a],[],[]]).
+test(t8, [fail]) :- checkFinDiagonale2(1,4,[[a,a,b,a],[a,a,a],[a,a],[a],[],[]]).
 
 % ---------------- Test finJeu ----------------
 
-%test10 :- checkFinJeu(1,5,[[a,a,a,a] [] [] [] [] []]).
-%test11 :- checkFinJeu(1,1,[[a] [] [] [] [] []]).
+test(t9) :- checkFinJeu(1,[[a,a,a,a],[],[],[],[],[]]).
+test(t10, [fail]) :- checkFinJeu(1,[[a],[],[],[],[],[]]).
 
+:-end_tests(finJeu).
